9d8138e359f6fc000c14939c6bd059149ea92933
Oscar Daniel Amaya Santana <oscar.amaya@eia.edu.co>
2024-03-06 20:01:46 -0500

    grafica dots

---
+++ components/Graph/Graph.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from "recharts";
+
+export default function Graph({data}) {
+  
+    return (
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date_time" />
+        <YAxis />
+  
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    );
+  }
+
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,30 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header/header";
 import Sidemenu from "../components/Sidemenu/sidemenu";
-export default function Home() {
+import Graph from "../components/Graph/Graph";
+
+export default function Home({list_dots}) {
   return (
     <div className={styles.main}>
       <Header />
-      <Sidemenu />
-      <div className={styles.title}>
-        Home</div>
+      <div className={styles.container}>
+        <Sidemenu />
+        <div className={styles.graphContainer}>
+          <Graph data={list_dots?.dots}/>
+        </div>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const apiresponse = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "/dots/5/"
+  );
+  const list_dots = await apiresponse.json();
+  return {
+    props: {
+      list_dots,
+    },
+  };
+};
